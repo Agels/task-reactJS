e97be3bd47aa8e5d5785f8fbf54cac0991360fd5
Agels <ardhianagilr@gmail.com>
2022-04-04 14:03:57 +0700

    add app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          first task react
+          first task reactJS
         </p>
         <a
           className="App-link"
